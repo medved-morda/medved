b1754f5fc71de686c5ea4318f6f3b96f1ea284e7
medved <incredible.medved@gmail.com>
2023-03-09 21:08:59 +0700

    test

--- src/self/snake/Const.groovy
+++ src/self/snake/Const.groovy
@@ -4,7 +4,7 @@ class Const {
     public static int CELL_AMT = 20
     public static int CELL_SIZE = 25
    // public static int FIELD_WIDTH = 100, FIELD_HEIGHT = 100
-    public static int SNAKE_INIT_LENGTH = 5
+    public static int SNAKE_INIT_LENGTH = 6
     public static int DELAY = 100
 
 }
